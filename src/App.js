36e1a1b75688400dc05ed0b223d90a1157e98cdc
SurrealShock <Adrian_Avram@Outlook.com>
2018-08-17 10:19:32 -0700

    Added firebase auth

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import store from './store';
 import AddClient from './components/clients/AddClient';
 import ClientDetails from './components/clients/ClientDetails';
 import EditClient from './components/clients/EditClient';
+import Login from './components/auth/Login';
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Dashboard} />
+                <Route exact path="/login" component={Login} />
                 <Route exact path="/client/add" component={AddClient} />
                 <Route exact path="/client/:id" component={ClientDetails} />
                 <Route exact path="/client/edit/:id" component={EditClient} />
--- src/components/layout/AppNavBar.js
+++ src/components/layout/AppNavBar.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firebaseConnect } from 'react-redux-firebase';
+import Spinner from '../layout/spinner';
+import classnames from 'classnames';
 
 class AppNavBar extends Component {
+  state = {
+    isAuthenticated: false
+  };
+
+  onLogout = e => {
+    e.preventDefault();
+
+    const { firebase } = this.props;
+    firebase.logout();
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    const { auth } = props;
+
+    if (auth.uid) {
+      return { isAuthenticated: true };
+    } else {
+      return { isAuthenticated: false };
+    }
+  }
+
   render() {
+    const { isAuthenticated } = this.state;
+
     return (
       <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
         <div className="container">
@@ -19,12 +47,26 @@ class AppNavBar extends Component {
           </button>
           <div className="collapse navbar-collapse" id="navbarMain">
             <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  Dashboard
-                </Link>
-              </li>
+              {isAuthenticated ? (
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">
+                    Dashboard
+                  </Link>
+                </li>
+              ) : null}
             </ul>
+            {isAuthenticated ? (
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <a href="#!" className="nav-link">
+                    {this.props.auth.email}
+                  </a>
+                </li>
+                <a onClick={this.onLogout} href="#!" className="nav-link">
+                  Logout
+                </a>
+              </ul>
+            ) : null}
           </div>
         </div>
       </nav>
@@ -32,4 +74,9 @@ class AppNavBar extends Component {
   }
 }
 
-export default AppNavBar;
+export default compose(
+  firebaseConnect(),
+  connect((state, props) => ({
+    auth: state.firebase.auth
+  }))
+)(AppNavBar);
